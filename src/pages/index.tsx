b5829dcb15e8e975be1392a956ca4701bc3468f1
Bartosz Gargol <bgargol@7bulls.com>
2023-04-25 15:24:39 +0200

    prisma update web posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const {data} = api.posts.getAll.useQuery()
 
   return (
     <>
@@ -25,6 +25,9 @@ const Home: NextPage = () => {
           <SignedOut>
             <SignInButton />
           </SignedOut>
+        </div>
+        <div>
+          {data?.map(post=>(<div key={post.id}>{post.content}</div>))}
         </div>
       </main>
     </>
